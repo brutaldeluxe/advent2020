78d2deb4d602f9391425b1d249978b3df4758d0b
Jonatan Flarup <jonatanflarup@gmail.com>
2020-12-14 19:27:51 +0100

    Day 12 part 2

--- src/day12.js
+++ src/day12.js
@@ -7,49 +7,50 @@ const loadData = () => {
 
 const directions = loadData()
 
-let direction = 90
-let posX = 0
-let posY = 0
+let pos = {x:0, y:0}
+let wayP = {x:10, y:1}
 directions.forEach(line => {
     let match = line.match(/([A-Z])(\d*)/)
     let action = match[1]
     let value = Number.parseInt(match[2])
-    console.log(line)
 
+    let x = wayP.x
+    let y = wayP.y
+    let rad = 0
     switch(action){
-        case 'N': posY+=value 
+        case 'N': 
+            wayP.y+=value
             break
-        case 'S': posY-=value
+        case 'S': 
+            wayP.y-=value
             break
-        case 'E': posX+=value
+        case 'E': 
+            wayP.x+=value
             break
-        case 'W': posX-=value
+        case 'W': 
+            wayP.x-=value
             break
-        case 'L': direction = (360+direction - value)%360
+        case 'L': 
+            rad = -value*Math.PI/180;
+            wayP.x = Math.round(x * Math.cos(rad) + y * Math.sin(rad))
+            wayP.y = Math.round(-x * Math.sin(rad) + y * Math.cos(rad))
             break
-        case 'R': direction = (direction + value)%360
+        case 'R': 
+            rad = value*Math.PI/180
+            wayP.x = Math.round(x * Math.cos(rad) + y * Math.sin(rad))
+            wayP.y = Math.round(-x * Math.sin(rad) + y * Math.cos(rad))
             break
         case 'F': 
-            if(direction == 0){
-                posY+=value
-            }
-            if(direction == 90){
-                posX+=value
-            }
-            if(direction == 180){
-                posY-=value
-            }
-            if(direction == 270){
-                posX-=value
-            }
+            pos.x+=value*wayP.x
+            pos.y+=value*wayP.y
             break
         default: console.log('Hey now. Total error '+action)
     }
-    console.log({posX, posY, direction})
+    //console.log({pos, wayP})
 })
 
-const manhattanPos = (x, y) => {
-    return Math.abs(x) + Math.abs(y)
+const manhattanPos = (pos) => {
+    return Math.abs(pos.x) + Math.abs(pos.y)
 }
 
-console.log(manhattanPos(posX, posY))
+console.log(manhattanPos(pos))
