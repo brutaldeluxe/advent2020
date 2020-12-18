4318472144658eb29137fd5614216ebe86ce3b3a
Jonatan Flarup <jonatanflarup@gmail.com>
2020-12-18 10:08:10 +0100

    Day 15 part 2

--- src/day15.js
+++ src/day15.js
@@ -4,7 +4,7 @@ fs.readFile('./data/day15_data', 'utf8', (err, data) => {
     data = Array.from(data.split(','), number => {
         return Number.parseInt(number)
     })
-    console.log('question 1: ', partOne(data))
+    console.log('question 1: ', partOneMap(data))
 })
 
 const partOne = (data) => {
@@ -21,4 +21,27 @@ const partOne = (data) => {
     }
     
     return data[data.length-1]
+}
+
+const partOneMap = (data) => {
+    let map = new Map()
+    data.forEach((n, i) => {
+        map.set(n, i+1)
+    })
+
+    let lastNum = 0
+    
+    for(let i = data.length+1; i<30000000; i++){ 
+        if(map.has(lastNum)){
+            let temp = map.get(lastNum)
+            map.set(lastNum, i)
+            lastNum = i-temp
+        }
+        else {
+            map.set(lastNum, i)
+            lastNum = 0
+        }
+    }
+
+    return lastNum
 }
